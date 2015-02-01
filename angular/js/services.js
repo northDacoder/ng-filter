654ab99bbb28c441a319764d7ac05a00166a5d2d
John Ryan <northdacoder@gmail.com>
2015-01-31 23:48:21 -0600

    add angular dependencies & setup angular module

---
+++ angular/js/services.js
@@ -0,0 +1,11 @@
+angular.module('movieApp.services',[]).factory('Movie',function($resource){
+    return $resource('/api/movies/:id',{id:'@_id'},{
+        update: {
+            method: 'PUT'
+        }
+    });
+}).service('popupService',function($window){
+    this.showPopup=function(message){
+        return $window.confirm(message);
+    }
+});
